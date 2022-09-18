92bba1314677b80bddaa267b934def74db064940
katrin <slavnova.katya@gmail.com>
2022-09-18 12:51:22 +0300

    Запрос 3х чисел и определение max

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите 1 число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max= ");
+Console.WriteLine(max);
