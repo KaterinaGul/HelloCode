a2307a6a15d64556290633d07f745f4e22cff643
katrin <slavnova.katya@gmail.com>
2022-09-23 11:40:35 +0300

    Третье домашнее задание

---
+++ HW3/Task21/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координату x для первой точки");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y для первой точки");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z для первой точки");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату x для второй точки");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y для второй точки");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z для второй точки");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double l = Math.Sqrt((xb-xa)*(xb-xa) + (yb-ya)*(yb-ya) + (zb-za)*(zb-za));
+Console.WriteLine($"Расстояние между точками {Math.Round(l,2)}");
---
+++ HW3/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int number =1;
+
+while (count <= N)
+{
+    number = count*count*count;
+    count++;
+    Console.Write($"{number}, ");
+}
