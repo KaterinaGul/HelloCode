5a250e9a23de1622ed2f18f72147effc61f12048
katrin <slavnova.katya@gmail.com>
2022-10-13 14:12:22 +0300

    Завершающая домашняя работа по курсу

---
+++ HW9/Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string NumbersRec(int a, int n)
+{
+    if (a <= n) return NumbersRec(a + 1, n) + $"{a} ";
+    else return String.Empty;
+}
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+Console.WriteLine(NumbersRec(a, n));
